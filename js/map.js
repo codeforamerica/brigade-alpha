e40f498bc728bdd9a5909c09ad8db1bfe8543405
Frances Berriman <fberriman@gmail.com>
2014-02-18 14:51:42 -0800

    Remove some dead code

--- js/map.js
+++ js/map.js
@@ -13,51 +13,37 @@ $(function(){
 
 
   var lat_lngs = [];
-  $.getJSON("http://brigade.codeforamerica.org/brigades.json", function(data){
-  	for(d in data){
-			var brigade = data[d]
-			if(brigade.location){
-				if(brigade.location.latitude && brigade.location.longitude){
-					var lat = brigade.location.latitude,
-							lng = brigade.location.longitude;
+	  $.getJSON("http://brigade.codeforamerica.org/brigades.json", function(data){
+	  	for(d in data){
+				var brigade = data[d]
+				if(brigade.location){
+					if(brigade.location.latitude && brigade.location.longitude){
+						var lat = brigade.location.latitude,
+								lng = brigade.location.longitude;
 
-					lat_lngs.push([lat,lng])
-					marker = L.marker(new L.LatLng(lat,lng), {
-				  	icon: L.mapbox.marker.icon({'marker-symbol': 'town-hall'})
-					});
+						lat_lngs.push([lat,lng])
+						marker = L.marker(new L.LatLng(lat,lng), {
+					  	icon: L.mapbox.marker.icon({'marker-symbol': 'town-hall'})
+						});
+						
+
+						// go through each item and create an appropriate overlay
+						var html = "<a href="+brigade.group_url+"><button>"+brigade.name+"</button></a><input type='hidden' value='"+brigade.id+"' />";
+						html += $(".brigade-pin-overlay").html();
+						marker.bindPopup(html);
+						createOverlay(brigade.id, brigade.name);
+						map.addLayer(marker);
 					
-					/*var html = "<a href="+brigade.group_url+"><button>Visit "+brigade.name+"</button></a>";
-					// html += "<button class='join'>Join "+brigade.name+"</button>";
-					$("#brigade_id").html("<option value="+brigade.id+"></option>");
-					html += $("#brigade-overlay").html();
-					//marker.bindPopup(html);*/
+				};
 
-					// go through each item and create an appropriate overlay
-					var html = "<a href="+brigade.group_url+"><button>"+brigade.name+"</button></a><input type='hidden' value='"+brigade.id+"' />";
-					html += $(".brigade-pin-overlay").html();
-					marker.bindPopup(html);
-					createOverlay(brigade.id, brigade.name);
-					map.addLayer(marker);
-				
-			};
+				marker.on('click',function(e) {
+					showOverlay($(".leaflet-container input").val());
+				});
+			} 
 
-			marker.on('click',function(e) {
-				showOverlay($(".leaflet-container input").val());
-			});
-		} 
+		}
 
-	}
-
-	
-	
-
-	// Hide join button when clicked, show form
-	// $("#join").click(function(){
-	// 	$("#new_user").html();
-	// 	$("#join").hide();
-	// })
-
-  });
+ 	});
 
 /*
 	init = function(){
