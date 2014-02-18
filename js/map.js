bc22b1fde089217a594545d7ed4662f8bcaf225b
Frances Berriman <fberriman@gmail.com>
2014-02-18 10:35:01 -0800

    Understanding how mapbox works

--- js/map.js
+++ js/map.js
@@ -1,46 +1,55 @@
 $(function(){
 
-	
+
   $('#map').css("height", ($(window).height() - 150));
+
   var map = L.mapbox.map('map', 'codeforamerica.map-hhckoiuj', 
   	{
   		scrollWheelZoom:false
   	}
-  	).setView([35, -100], 3)
-  ;
+  	).setView([35, -100], 3);
+
   map.zoomControl.setPosition('bottomleft');
 
 
   var lat_lngs = [];
   $.getJSON("http://brigade.codeforamerica.org/brigades.json", function(data){
   	for(d in data){
-		var brigade = data[d]
-		if(brigade.location){
-			if(brigade.location.latitude && brigade.location.longitude){
-				var lat = brigade.location.latitude;
-				var lng = brigade.location.longitude;
-				lat_lngs.push([lat,lng])
-				marker = L.marker(new L.LatLng(lat,lng), {
-				  icon: L.mapbox.marker.icon({'marker-symbol': 'town-hall'})
-				})
-				var html = "<a href="+brigade.group_url+"><button>Visit "+brigade.name+"</button></a>";
-				// html += "<button class='join'>Join "+brigade.name+"</button>";
-				$("#brigade_id").html("<option value="+brigade.id+"></option>");
-				html += $("#brigade-overlay").html();
-				marker.bindPopup(html);
+			var brigade = data[d]
+			if(brigade.location){
+				if(brigade.location.latitude && brigade.location.longitude){
+					var lat = brigade.location.latitude,
+							lng = brigade.location.longitude;
+
+					lat_lngs.push([lat,lng])
+					marker = L.marker(new L.LatLng(lat,lng), {
+				  	icon: L.mapbox.marker.icon({'marker-symbol': 'town-hall'})
+					});
+					
+					/*var html = "<a href="+brigade.group_url+"><button>Visit "+brigade.name+"</button></a>";
+					// html += "<button class='join'>Join "+brigade.name+"</button>";
+					$("#brigade_id").html("<option value="+brigade.id+"></option>");
+					html += $("#brigade-overlay").html();
+					//marker.bindPopup(html);*/
+
+					// go through each item and create an appropriate overlay
+
+					console.log(marker);
+					console.log(brigade.id);
 
 
-
-				//$("#brigade-overlay").show();
-				map.addLayer(marker);
-				console.log(html);
+					map.addLayer(marker);
 				
-			}
-		} 
+			};
 
+			marker.on('click',function(e) {
+				showOverlay(this._leaflet_id);
+			});
+		} 
 
 	}
 
+	
 	
 
 	// Hide join button when clicked, show form
@@ -50,9 +59,12 @@ $(function(){
 	// })
 
   });
+	init = function(){
+		setup();
+	};
 
 	setup = function(){
-		$("#brigade-overlay").css("display", "none");
+		$(".brigade-overlay").css("display", "none");
 	};
 
 	cleanup = function(){
@@ -61,9 +73,13 @@ $(function(){
 	};
 
 	createOverlay = function(){
-		// create custom 
-		$("#brigade-overlay").css("display", "block");
+		var overlayHTML = "<div class='"+id+" brigade-overlay'>Brigade"+id+"</div>";
+
 	};
 
+	showOverlay = function(id){
+		console.log(id);
+		$("."+id).css("display", "block");
+	};
 
 });
