68c57d5bf828a58f70071030b33e90731d6faede
Andrew Hyder <hyd415@gmail.com>
2014-03-15 18:54:38 -0700

    WIP

--- js/map.js
+++ js/map.js
@@ -1,76 +1,105 @@
 $(function(){
 
+  // Leave some room for the header and footer
   $('#map').css("height", ($(window).height() - 180));
+  $('#overlay').css("height", ($(window).height() - 243));
 
   var map = L.mapbox.map('map', 'codeforamerica.map-hhckoiuj', 
-  	{
-  		scrollWheelZoom:false
-  	}
-  	).setView([35, -100], 3);
+    {
+      scrollWheelZoom:false
+    }
+    ).setView([35, -100], 3);
 
   map.zoomControl.setPosition('bottomright');
 
   cfapi = "http://civic-tech-movement.codeforamerica.org/api/organizations"
   // cfapi = "http://localhost:5000/api/organizations"
 
-  var lat_lngs = [];
-  $.getJSON(cfapi, function(data){
-  	data = data.objects;
-  	for(d in data){
-		var brigade = data[d]
-		console.log(brigade);
-		if (brigade.type == "Brigade"){
-			if (brigade.latitude && brigade.longitude){
-				var lat = brigade.latitude,
-						lng = brigade.longitude;
 
-				lat_lngs.push([lat,lng])
-				marker = L.marker(new L.LatLng(lat,lng), {
-			  	icon: L.mapbox.marker.icon({'marker-symbol': 'town-hall'})
-				});
-
-				// go through each item and create an appropriate overlay
-				var html = "<a href="+brigade.website+"><button>"+brigade.name+"</button></a><input type='hidden' value='"+brigade.id+"' />";
-				html += $(".brigade-pin-overlay").html();
-				marker.bindPopup(html);
-				createOverlay(brigade.id, brigade.name);
-				map.addLayer(marker);
-			
-			};
-		}
-			marker.on('click',function(e) {
-				showOverlay($(".leaflet-container input").val());
-			});
-		} 
-  }).done( function() {
-	// This uses the HTML5 geolocation API, which is available on
-	// most mobile browsers and modern browsers, but not in Internet Explorer
-	//
-	// See this chart of compatibility for details:
-	// http://caniuse.com/#feat=geolocation
-	if (!navigator.geolocation) {
-	    console.log('geolocation is not available');
-	} else {
-		map.locate({setView:true, maxZoom:4});
-	}
+  // Custom marker type with space for Brigade data
+  var BrigadeMarker = L.Marker.extend({
+    options: {
+      brigade : {}
+    }
   });
 
-	createOverlay = function(id, name){
+  $.getJSON(cfapi, function(data){
+    var brigades = data.objects;
 
-		var content = $(".brigade-overlay-content").html();
-		var overlayHTML = "<div class='"+id+" brigade-overlay slab-red'><a class='close-overlay' href='#'>Close</a><div class='badge-heading'><h2>"+name+"</h2></div>"+content+"</div>";
-		$("body").append(overlayHTML);
-		$(".close-overlay").on("click", function(){ hideOverlay(); return false; })
+    $.each(brigades, function(i, brigade){
+      console.log(brigade);
 
-	};
+      if (brigade.type == "Brigade"){
+        if (brigade.latitude && brigade.longitude){
+          var lat = brigade.latitude, lng = brigade.longitude;
 
-	showOverlay = function(id){
-		hideOverlay();
-		$("."+id).toggle();
-	};
+          var marker = new BrigadeMarker(new L.LatLng(lat,lng), {
+            icon: L.mapbox.marker.icon({'marker-symbol': 'town-hall'}),
+            brigade: brigade // Add Brigade data to marker
+          });
 
-	hideOverlay = function(){
-		$(".brigade-overlay").css("display", "none");
-	};
+          map.addLayer(marker);
+
+          // On click, show the Brigade data on the overlay
+          marker.on('click',function(e) {
+            updateOverlay(e.target.options.brigade);
+          });
+
+        }
+      }
+    });
+  }).done( function() {
+  // This uses the HTML5 geolocation API, which is available on
+  // most mobile browsers and modern browsers, but not in Internet Explorer
+  //
+  // See this chart of compatibility for details:
+  // http://caniuse.com/#feat=geolocation
+  if (!navigator.geolocation) {
+      console.log('geolocation is not available');
+  } else {
+    map.locate({setView:true, maxZoom:4});
+  }
+  });
+
+  function updateOverlay(brigade){
+    $("#brigade-name").text(brigade.name);
+    $("#brigade-url").text(brigade.website).attr("href",brigade.website).show();
+    $("#program-info").hide();
+
+    for (list in [brigade.stories, brigade.projects, brigade.events]){
+      console.log(list);
+    }
+
+    if (brigade.stories) {
+      $("#stories").show();
+      $.each(brigade.stories, function(i, story){
+        html = "<li><a href="+story.link+">"+story.title+"</li>"
+        $("#stories ul").append(html);
+      })
+    } else {
+      $("#stories").hide();
+    }
+
+    if (brigade.projects) {
+      $("#projects").show();
+      $.each(brigade.projects, function(i, project){
+        html = "<li><a href="+project.link+">"+project.title+"</li>"
+        $("#projects ul").append(html);
+      })
+    } else {
+      $("#projects").hide();
+    }
+
+    if (brigade.events) {
+      $("#events").show();
+      $.each(brigade.events, function(i, event){
+        html = "<li><a href="+event.link+">"+event.title+"</li>"
+        $("#events ul").append(html);
+      })
+    } else {
+      $("#events").hide();
+    }
+    
+  }
 
 });
