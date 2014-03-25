a812906655f5cf3951be68863f758571ba524671
Michal Migurski <mike@teczno.com>
2014-03-25 11:16:31 -0700

    Added brigade-to-HTML converter script

---
+++ brigade.php
@@ -0,0 +1,51 @@
+<?php
+
+    $ctm_api_base = 'http://civic-tech-movement.codeforamerica.org/api';
+    $brigade_name = rawurlencode(ltrim($_SERVER['PATH_INFO'], '/'));
+    $brigade_url = "{$ctm_api_base}/organizations/{$brigade_name}";
+    $info = json_decode(file_get_contents($brigade_url), true);
+    
+    header('Content-Type: text/html');
+    
+    function h($s) 
+    {
+        return htmlspecialchars($s);
+    }
+
+?>
+<div id="brigade-info">
+	<h2 id="brigade-name"><?= h($info['name']); ?></h2>
+	<p>
+        <a id="brigade-url" href="<?= h($info['website']); ?>" style="display: inline;"><?= h($info['website']); ?></a>
+	</p>
+	<p id="program-info" style="display: none;">
+		The Brigade program helps local volunteer groups partner with government in an effort to enhance their communities. Brigades hold regular hack nights, events, advocate for open data, and deploy apps.
+	</p>
+	<div id="stories" style="display: block;">
+		<b>Recent Stories</b>
+		<ul>
+		    <? foreach(array_slice($info['stories'], 0, 2) as $s) { ?>
+                <li><a href="<?= h($s['link']) ?>"><?= h($s['title']) ?></a></li>
+			<? } ?>
+		</ul>
+	</div>
+	<div id="events" style="display: block;">
+		<b>Events</b>
+		<ul>
+		    <? foreach(array_slice($info['events'], 0, 2) as $e) { ?>
+                <li><a href="<?= h($e['event_url']) ?>"><?= h($e['name']) ?></a></li>
+			<? } ?>
+		</ul>
+	</div>
+	<div id="projects" style="display: block;">
+		<b>Recent Projects</b>
+		<ul>
+		    <? foreach(array_slice($info['projects'], 0, 2) as $p) { ?>
+                <li>
+                    <a href="<?= h($p['link_url']) ?>"><?= h($p['name']) ?></a>
+                    <br><?= h($p['description']) ?>
+                </li>
+			<? } ?>
+		</ul>
+	</div>
+</div>
