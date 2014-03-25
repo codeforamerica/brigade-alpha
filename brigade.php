8a5e6b35bca693d0ab7a147a707f941bada8403e
Michal Migurski <mike@teczno.com>
2014-03-25 11:29:06 -0700

    Updated brigade HTML to omit events/stories/etc. when absent

--- brigade.php
+++ brigade.php
@@ -18,34 +18,42 @@
 	<p>
         <a id="brigade-url" href="<?= h($info['website']); ?>" style="display: inline;"><?= h($info['website']); ?></a>
 	</p>
-	<p id="program-info" style="display: none;">
+	<!--
+	<p id="program-info">
 		The Brigade program helps local volunteer groups partner with government in an effort to enhance their communities. Brigades hold regular hack nights, events, advocate for open data, and deploy apps.
 	</p>
-	<div id="stories" style="display: block;">
-		<b>Recent Stories</b>
-		<ul>
-		    <? foreach(array_slice($info['stories'], 0, 2) as $s) { ?>
-                <li><a href="<?= h($s['link']) ?>"><?= h($s['title']) ?></a></li>
-			<? } ?>
-		</ul>
-	</div>
-	<div id="events" style="display: block;">
-		<b>Events</b>
-		<ul>
-		    <? foreach(array_slice($info['events'], 0, 2) as $e) { ?>
-                <li><a href="<?= h($e['event_url']) ?>"><?= h($e['name']) ?></a></li>
-			<? } ?>
-		</ul>
-	</div>
-	<div id="projects" style="display: block;">
-		<b>Recent Projects</b>
-		<ul>
-		    <? foreach(array_slice($info['projects'], 0, 2) as $p) { ?>
-                <li>
-                    <a href="<?= h($p['link_url']) ?>"><?= h($p['name']) ?></a>
-                    <br><?= h($p['description']) ?>
-                </li>
-			<? } ?>
-		</ul>
-	</div>
+	-->
+    <? if($info['stories']) { ?>
+        <div id="stories">
+            <b>Recent Stories</b>
+            <ul>
+                <? foreach(array_slice($info['stories'], 0, 2) as $s) { ?>
+                    <li><a href="<?= h($s['link']) ?>"><?= h($s['title']) ?></a></li>
+                <? } ?>
+            </ul>
+        </div>
+    <? } ?>
+    <? if($info['stories']) { ?>
+        <div id="events">
+            <b>Events</b>
+            <ul>
+                <? foreach(array_slice($info['events'], 0, 2) as $e) { ?>
+                    <li><a href="<?= h($e['event_url']) ?>"><?= h($e['name']) ?></a></li>
+                <? } ?>
+            </ul>
+        </div>
+    <? } ?>
+    <? if($info['stories']) { ?>
+        <div id="projects">
+            <b>Recent Projects</b>
+            <ul>
+                <? foreach(array_slice($info['projects'], 0, 2) as $p) { ?>
+                    <li>
+                        <a href="<?= h($p['link_url']) ?>"><?= h($p['name']) ?></a>
+                        <br><?= h($p['description']) ?>
+                    </li>
+                <? } ?>
+            </ul>
+        </div>
+    <? } ?>
 </div>
