8ab5a6cfe32a4b0b0bdf48ad6239418fee5ee607
Andrew Hyder <hyd415@gmail.com>
2014-03-28 15:53:49 -0700

    no_brigade and organizer form working.

--- overlay-home.php
+++ overlay-home.php
@@ -5,6 +5,7 @@
 
   <div id="join-form">
     <form accept-charset="UTF-8" action="http://brigade.codeforamerica.org/members" id="new_user" method="post" novalidate="novalidate">
+      <input id="no_brigade" type="hidden" name="source" value="no_brigade">
       <h3>Want to get connected?</h3>
       <ul class="list-form">
         <li class="form-field">
@@ -16,24 +17,30 @@
             <input id="user_email" name="user[email]" type="text" placeholder="benfranklin@codeforamerica.org">
         </li>
         <li class="form-field">
-
-        <select id="brigade_id" name="brigade_id" style="display:none;">
-        </select>
-
-
-        <div>
-        <input name="user[work_in_government]" type="hidden" value="0">
-
-        <label for="user_work_in_government">
-            <input id="user_work_in_government" name="user[work_in_government]" type="checkbox" value="1">
-            I work in government
+            <label for="user_work_in_geovernment"><input id="user_work_in_government" name="user[work_in_government]" type="checkbox" value="1">I work in government</label>
         </li>
+        <li class="form-field">
+            <label for="source"><input id="organizer" value="organizer" type="checkbox">I want to Organize!</label>
+        </li>
+        <select id="user_location_id" name="user[location_id]" style="display:none;">
+          <option value></option>
+        </select>
       </ul>
-
-
       <input id="user_human_check" name="user[human_check]" size="50" type="hidden">
-      <input type="hidden" name="source" value="brigade">
       <input name="utf8" type="hidden" value="✓">
       <input name="commit" type="submit" value="Submit">
     </form>
   </div>
+
+  <script>
+    $('#organizer').bind('change', function(){
+      if ($('#organizer').is(':checked')) {
+        $("#no_brigade").attr("name",null);
+        $("#organizer").attr("name","source");
+      } else {
+        $("#no_brigade").attr("name","source");
+        $("#organizer").attr("name",null);
+      }
+      // console.log("WHAT");
+    });
+  </script>
