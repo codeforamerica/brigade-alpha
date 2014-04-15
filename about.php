0e219675d4be2485a0f4865bd934f81c943b938d
Andrew Hyder <hyd415@gmail.com>
2014-04-15 11:33:44 -0700

    Updated About page.

--- about.php
+++ about.php
@@ -1,57 +1,61 @@
 <?
 
     include('_layouts/top.php');
-    
+
     $base_url = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
 
 ?>
 <div class="layout-semibreve">
 
-  <h1>Brigade Activities</h1>
+  <h1>About</h1>
 
-  <h2>Goals</h2>
-  <ol>
+  <p id="program-info">The Code for America Brigade program is an international network of people committed to using their voices and hands, in collaboration with local governments, to make their cities better.</p>
 
-    <li>Governments leveraging technology more effectively</li>
-    <li>Citizens and community groups solving civic issues</li>
-    <li>Cities collaborating to work better</li>
-    <li>Civic apps built on linked, open data</li>
-  
-  </ol>
+  <p>Brigades build participatory power in their cities by holding regular civic hack nights and events, advocating for open data, and deploying apps. Brigades celebrate and share their successes with the Brigade network to further the global civic innovation movement. </p>
 
-  <h2>Mission</h2>
-  <p>The mission of any local CfA Brigade is to deploy, maintain, and sustain civic technology and open data infrastructure for a place.</p>
-  <ul>
 
-    <li>We want to make it easy for people to code for America.</li>
-    <li>We want to make it easy for places to adopt modern civic technology.</li>
-    <li>We want to make it easy for places to adopt open data infrastructure.</li>
-    <li>We want to innovate in ways that improve the sustainability of civic technology and infrastructure.</li>
-    <li>We want to fulfill the overall Code for America mission to “improve the way governments work better for everyone using the people and the power of the web.”</li>
-
+  <ul id="faq" class="list-no-bullets">
+    <h2>FAQ</h2>
+    <li>
+      <blockquote>Can I participate in a Brigade if I’m not technical?</blockquote>
+      <p>Yes! Brigades need leaders and members with all types of skillsets. Some ways non-technical folks participate include storytelling, community organizing, advising on projects as a subject matter experts, planning events, writing copy, and more. Find out more about <a href="https://docs.google.com/a/codeforamerica.org/document/d/1oEB1CuzCHldxP_bzcvmcrmK0yAE1BzV2AGhtwtYKZVY/edit">Roles within a Local Brigade</a> or work with your local Brigade leader or Captain to see how to best plug in.</p>
+    </li>
+    <li>
+      <blockquote>What if there isn’t a Brigade where I live?</blockquote>
+      <p>If there isn’t a Brigade where you live, <a href="http://codeforamerica.org/brigade/">sign up to lead one</a>. If you’re not interested in leading a Brigade, you can stay in the loop about global Brigade activity by signing up for the <a href="http://codeforamerica.org/brigade/">Brigade listserv</a>.</p>
+    </li>
+    <li>
+      <blockquote>When does a Brigade become official?</blockquote>
+      <p>Code for America recognizes a Brigade as official when it is led by a Captain. A Code for America Brigade Captain is a volunteer who has demonstrated commitment as a Brigade leader. Brigade Captains are recognized by Code for America after achieving a number of organizing milestones.</p>
+    </li>
+    <li>
+      <blockquote>How can I get my Brigade listed on the Brigade website?</blockquote>
+      <p>Code for America will list Brigades on the Brigade website after the leader attends an orientation, finds people to fill Core Member roles, and the core team completes and submits a City Profile and Strategic Plan to Brigade program staff.</p>
+    </li>
+    <li>
+      <blockquote>How can I stay plugged into the global Brigade network?</blockquote>
+      <p>Sign up for the <a href="http://codeforamerica.org/brigade/">Brigade listserv</a>.</p>
+    </li>
+    <li>
+      <blockquote>How can I participate if I’m not in the United States?</blockquote>
+      <p>The Brigade program is global. If there is a Brigade where you live, sign up to join. If not, you can <a href="http://codeforamerica.org/brigade/">sign up to lead one</a>.</p>
+    </li>
+    <li>
+      <blockquote>Do Brigade projects have to be free and open source?</blockquote>
+      <p>Brigade activities build power and tools that are open and free technology for public use. Brigade events are not places to pitch startup ideas. Brigades prioritize activities for public use, not private gain. <a href="https://www.fsf.org/about/what-is-free-software">See this explanation of free software for more information</a>.</p>
+    </li>
+    <li>
+      <blockquote>How does Code for America provide financial support for Brigades?</blockquote>
+      <p>Code for America provides different levels of financial support for Brigades depending on their level of development.</p>
+    </li>
+    <li>
+      <blockquote>Can my Brigade have more than one Municipal Partner?</blockquote>
+      <p>Yes! The more the merrier.</p>
+    </li>
   </ul>
-
-  <h2>Purpose</h2>
-  <p>The purpose of Brigade is to build local communities who sustain Code for America's goals and strategic outcomes</p>
-
-  <h2>FAQ</h2>
-  <h3>How can I participate if I’m not a coder or designer?</h3>
-  <p>You don’t have to be a coder or a designer to participate in a CfA Brigade. Here’s a sample list of other Brigade activities in which you can participate:</p>
-  <ul>
-
-    <li>Draft and publish open data strategy for your city</li>
-    <li>Source challenges by conducting interviews, creating and publishing challenge briefs</li>
-    <li>Curate apps and organizations in the Civic Commons Marketplace</li>
-    <li>Liberate public data from closed systems</li>
-    <li>Raise funds for an app or a Brigade</li>
-
-  </ul>
-
-  <h2>Contact</h2>
-  <p>Code for America’s Brigade support team is ready to help you determine the best approach for your city. <a href="mailto:brigade-info@codeforamerica.org">Contact us for assistance.</a></p>
 
   <h2>License</h2>
-  <p>Brigade is licensed under <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons, Attribution, Non-Commercial, No-Derivs.</a></p>
+  <p>Brigade is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons, Attribution, Non-Commercial, Share-Alike</a></p>
 
 </div>
 <? include('_layouts/bottom.php') ?>
