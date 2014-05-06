97e348a464d60d02ffe57de322773b5c0cac5ae4
Andrew Hyder <hyd415@gmail.com>
2014-05-06 16:32:52 -0700

    Added a few more tools.

--- tools.php
+++ tools.php
@@ -1,14 +1,14 @@
 <?
 
     include('top.php');
-    
+
     $base_url = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
 
 ?>
 <section>
-  <div class="layout-breve">      
+  <div class="layout-breve">
     <div class="layout-crotchet-staccato">
-      <h3><strong>Getting started</strong></h3>      
+      <h3><strong>Getting started</strong></h3>
       <ul class="list-ruled">
           <li><a href='https://docs.google.com/document/d/1usHMpLu4FBdMTpfmQeQYz4Y4hfW-LXbGtTNpnMw20R4/edit?usp=sharing'>Introduction</a></li>
           <!--<li><a href='https://docs.google.com/document/d/1OH-rmKsuMoSj6FCR8H89TlR08co0WwAv28ISUT277nY/edit?usp=sharing'>How To: Get Started</a></li>-->
@@ -21,33 +21,47 @@
           <li><a href='https://docs.google.com/document/d/1fR-uImfI3zbH__BhEzlsTgsKxgs21FK4C6zIwgeuWGw/edit?usp=sharing'>Websites, Meetups, and Social Media</a></li>
           <li><a href='https://docs.google.com/document/d/1r0aEcMwpMSwo4P-x6PJ5WV5y1ouciOkvRTiGATFJAmU/edit?usp=sharing'>Template: City Profile / City Assessment Document</a></li>
           <li><a href='https://docs.google.com/document/d/1jTl0jRtyfPvlxweDuUSygnpo7ZG-rRV5lDnYpTLwbsI/edit?usp=sharing'>Template: Strategic Plan</a></li>
-      </ul>        
-    </div>        
-        
+      </ul>
+    </div>
+
     <div class="layout-crotchet-staccato">
-      <h3><strong>Marketing</strong></h3>      
+      <h3><strong>Marketing</strong></h3>
       <ul class="list-ruled">
           <li><a href='https://docs.google.com/document/d/1OY0s8BJR9A2aW1ivlNACTbi0dWOh9njYpHp53G1shFs/edit'>One Page Flyer</a> <a href='https://www.dropbox.com/s/d8omlxmg8g0gd1h/TemplateOnePager.pdf'>[PDF version]</a></li>
           <li><a href='https://docs.google.com/presentation/d/1MSyVMauTv4HcPcgXmF2l-Afzjw35UfieCj5mKDK7fvY/edit?usp=sharing'>Slide Deck</a> <a href='https://www.dropbox.com/s/t0ipq3okluv6p8l/Code%20for%20America%20Brigade%20-%20General%20Edition.pdf'>[PDF Version]</a> </li>
           <li><a href='https://www.dropbox.com/s/pkj0y9wt6fc4c6p/stickers.pdf' title='2.5 in sticker template'>Stickers</a></li>
           <li><a href='https://docs.google.com/document/d/1LIUcCLwmn-SR6_X93TOMIoIJOhNXd0t_27TAIOofdIo/edit?usp=sharing'>Style Guide</a></li>
       </ul>
-      <h4><strong>Logos</strong></h4>      
+      <h4><strong>Logos</strong></h4>
       <ul class="list-ruled">
           <li><a href='http://codeforamerica.org/media/images/logos/brigade.png'><img src='http://codeforamerica.org/media/images/logos/brigade.png' style='width: 270px; height: 102px;'></a></li>
           <li><a href='http://codeforamerica.org/wp-content/uploads/2013/04/tumblr_glasses.png'><img height='100' src='http://codeforamerica.org/wp-content/uploads/2013/04/tumblr_glasses.png' width='100'></a></li>
           <li><a href='http://www.codeforamerica.org/wp-content/uploads/2013/02/the_brigade.png'><img height='100' src='http://www.codeforamerica.org/wp-content/uploads/2013/02/the_brigade.png' width='300'></a></li>
           <li><a href='http://codeforamerica.org/logos/'>More Code for America logos</a></li>
-      </ul>   
+      </ul>
     </div>
     <div class="layout-crotchet-staccato">
-      <h3><strong>Events</strong></h3>      
+      <h3><strong>Events</strong></h3>
       <ul class="list-ruled">
           <li><a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.indd'>Name Tag Template</a> <a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.pdf'>[PDF version]</a></li>
           <li><a href='https://docs.google.com/open?id=0B_WC8QOgiwjzT05IdVdwSk5TX0E'>Meetup Sign</a></li>
       </ul>
-      <h4 class="h3">Brigade Sponsors</h4>
-       <p><a href='http://accela.com'><img src="images/accela.gif" width="300"></a></p>
+
+      <h3>Brigade Sponsors</h3>
+      <p><a href='http://accela.com'><img src="images/accela.gif" width="300"></a></p>
+
+      <h3>Free Stuff!</h3>
+      <h5>Just for Brigades with Captains</h5>
+      <ul>
+          <li><a href="http://aws.amazon.com/">AWS Credits</a></li>
+          <li><a href="http://www.justinmind.com/">JustInMind</a></li>
+      </ul>
+      <h5>For all Brigades</h5>
+      <ul>
+          <li><a href="https://community.socrata.com/">Socrata Community Data Portal</a></li>
+          <li><a href="https://cloud.google.com/developers/starterpack/">Google Cloud Platform</a></li>
+          <!-- <li><a href="#">OpenShift</a></li> -->
+      </ul>
     </div>
   </div>
 </section>
