a7463e2a76ed0fa60162ed95158c274158eba4ec
Michal Migurski <mike@teczno.com>
2014-10-29 13:33:58 -0700

    Revert "Messing with gov. WIP"
    This reverts commit f0938ea0b8561651d4d03faca7eef4c82c9dc810.

--- thank-you.php
+++ thank-you.php
@@ -6,7 +6,6 @@
     $is_specific_brigade = ($_GET['source'] == 'brigade' && $_GET['brigade_url']) ? true : false;
     $is_organizer = (!$is_specific_brigade && $_GET['source'] == 'organizer') ? true : false;
     $is_generic = (!$is_specific_brigade && !$is_organizer) ? true : false;
-    $works_in_gov = $_GET['work_in_government'] ? true : false;
 
     //
     // Get information about the selected brigade, if there is one.
@@ -25,13 +24,7 @@
 
 <div class="layout-semibreve">
 
-<? if($works_in_gov) { ?>
-
-  <div id="works_in_gov_text">
-    WHAT UP
-  </div>
-
-<? } elseif($is_specific_brigade) { ?>
+<? if($is_specific_brigade) { ?>
 
   <div id="brigade_text">
     <p><b>Thanks for signing up for <?= $brigade_info['name'] ?>.</b></p>
