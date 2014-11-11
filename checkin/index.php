8fd1aa5304c4a950f83476ab61b1b41d2f1e1f9b
Andrew Hyder <hyd415@gmail.com>
2014-11-11 11:50:47 -0800

    Checkins ready.

--- checkin/index.php
+++ checkin/index.php
@@ -74,9 +74,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     <label>Brigade</label><input type="text" name="brigade" placeholder="Code for San Francisco" value="<?echo htmlspecialchars($brigade);?>" />
                   </li>
                 </ul>
-                <input type=hidden name="date" placeholder=<? echo date('Y-m-d') ?> />
                 <button class="button-prominent button-l">Check In</button></li>
-                <p id="flag" style="float:right;display:none;">Thanks for checking in!</p>
               </form>
             </div>
           </div>
@@ -99,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         "name" => $_POST["name"],
         "email" => $_POST["email"],
         "event" => $_POST["event"],
-        "date" => $_POST["date"],
+        "date" => gmdate('Y-m-d'),
         "brigade" => $_POST["brigade"]
         );
 
@@ -123,10 +121,65 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
       $query = array("event" => $_POST["event"], "brigade" => $_POST["brigade"]);
       $redirect = sprintf("?%s", http_build_query($query));
-      
-      header('HTTP/1.1 303 See Other');
-      header("Location: {$redirect}");
-    }
+
+    ?>
+
+      <!DOCTYPE html>
+      <html lang="en-us">
+        <head>
+
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <title>Code for America - Check In</title>
+          <link rel="stylesheet" href="http://style.codeforamerica.org/style/css/main.css">
+          <link rel="stylesheet" href="http://style.codeforamerica.org/style/css/layout.css" media="all and (min-width: 40em)">
+
+          <!-- Need to use full link for hosting on gh-pages -->
+          <link rel="stylesheet" href="../css/style.css">
+
+          <link rel="apple-touch-icon-precomposed" href="/style/favicons/60x60/flag-red.png"/>
+
+        </head>
+        <body>
+        <div class="js-container">
+
+          <nav class="nav-global-primary">
+          </nav>
+
+            <div class="global-header">
+              <a href="<?= $base_url ?>/" class="global-header-logo">
+                  <img src="../images/logo.png" />
+              </a>
+              <p class="skip-to-nav"><a href="#global-footer">Menu</a></p>
+
+              <nav class="nav-global-secondary">
+                <ul>
+                  <li><a href="http://www.codeforamerica.org/about/brigade/">About</a></li>
+                  <li><a href="<?= $base_url ?>/tools">Tools</a></li>
+                  <!-- <li><a href="<?= $base_url ?>/events">Events</a></li> -->
+                  <li><a href="http://codeforamerica.tumblr.com">Tumblr</a></li>
+                  <li><a href="http://codeforamerica.org/donate" class="button">Donate</a></li>
+                </ul>
+              </nav>
+            </div>
+
+          <main role="main">
+
+
+            <section>
+              <div class="layout-semibreve">   
+                <div class="layout-gutter">
+                  <div class="layout-minim">
+                    <h1>Thanks</h1>
+                      <a href="<?= $redirect ?>"><button class="button-prominent button-l">New Check In</button></a>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+      <? 
+          include('../bottom.php');
+      }
 
 }
 
