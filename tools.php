e45b4950e69fd4571dc7d3c568a3b027e20277c1
Andrew Hyder <hyd415@gmail.com>
2014-12-08 15:16:26 -0800

    Rearrange Tools and added Twilio

--- tools.php
+++ tools.php
@@ -24,47 +24,50 @@
           <li><a href='https://docs.google.com/document/d/1jTl0jRtyfPvlxweDuUSygnpo7ZG-rRV5lDnYpTLwbsI/edit?usp=sharing'>Template: Strategic Plan</a></li>
 <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1RKkjW0YZfysBZ6JiJgV8j7o61H_Sh9y9pUl9fZzI2Bw/edit#heading=h.lfbjsp7i33tk'>Support Reimbursement Process</a></li>
       </ul>
+
+      <h3>Events</h3>
+      <ul class="list-ruled">
+          <li><a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.indd'>Name Tag Template</a> <a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.pdf'>[PDF version]</a></li>
+          <li><a href='https://docs.google.com/open?id=0B_WC8QOgiwjzT05IdVdwSk5TX0E'>Meetup Sign</a></li>
+      </ul>
+
     </div>
 
     <div class="layout-crotchet-staccato">
-      <h3><strong>For Your Website</strong></h3>
+      <h3>For Your Website</h3>
       <ul class="list-ruled">
         <li><a href="http://www.codeforamerica.org/brigade/form">A sign up form</a> - <a href="https://github.com/codeforamerica/brigade-alpha/tree/master/checkin#brigade-checkins">More info</a></li>
         <li><a href="http://www.codeforamerica.org/brigade/checkin/">An event check in tool</a> - <a href="https://github.com/codeforamerica/brigade-alpha#sign-ups">More info</a></li>
         <li><a href="http://www.codeforamerica.org/geeks/civicissues/embed">Embed your Brigade's GitHub Issues</a></li>
       </ul>
-      <h3><strong>Marketing</strong></h3>
+      <h3>Marketing</h3>
       <ul class="list-ruled">
           <li><a href='https://docs.google.com/document/d/1OY0s8BJR9A2aW1ivlNACTbi0dWOh9njYpHp53G1shFs/edit'>One Page Flyer</a> <a href='https://www.dropbox.com/s/d8omlxmg8g0gd1h/TemplateOnePager.pdf'>[PDF version]</a></li>
           <li><a href='https://docs.google.com/presentation/d/1MSyVMauTv4HcPcgXmF2l-Afzjw35UfieCj5mKDK7fvY/edit?usp=sharing'>Slide Deck</a> <a href='https://www.dropbox.com/s/t0ipq3okluv6p8l/Code%20for%20America%20Brigade%20-%20General%20Edition.pdf'>[PDF Version]</a> </li>
           <li><a href='https://www.dropbox.com/s/pkj0y9wt6fc4c6p/stickers.pdf' title='2.5 in sticker template'>Stickers</a></li>
           <li><a href='https://docs.google.com/document/d/1LIUcCLwmn-SR6_X93TOMIoIJOhNXd0t_27TAIOofdIo/edit?usp=sharing'>Style Guide</a></li>
       </ul>
-      <h4><strong>Logos</strong></h4>
+      <h3>Logos</h3>
       <ul class="list-ruled">
           <li><a href='http://codeforamerica.org/media/images/logos/brigade.png'><img src='http://codeforamerica.org/media/images/logos/brigade.png' style='width: 270px; height: 102px;'></a></li>
           <li><a href='http://codeforamerica.org/wp-content/uploads/2013/04/tumblr_glasses.png'><img height='100' src='http://codeforamerica.org/wp-content/uploads/2013/04/tumblr_glasses.png' width='100'></a></li>
           <li><a href='http://www.codeforamerica.org/wp-content/uploads/2013/02/the_brigade.png'><img height='100' src='http://www.codeforamerica.org/wp-content/uploads/2013/02/the_brigade.png' width='300'></a></li>
           <li><a href='http://codeforamerica.org/logos/'>More Code for America logos</a></li>
       </ul>
+
     </div>
     <div class="layout-crotchet-staccato">
-      <h3><strong>Events</strong></h3>
-      <ul class="list-ruled">
-          <li><a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.indd'>Name Tag Template</a> <a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.pdf'>[PDF version]</a></li>
-          <li><a href='https://docs.google.com/open?id=0B_WC8QOgiwjzT05IdVdwSk5TX0E'>Meetup Sign</a></li>
-      </ul>
-    <h4 class="h3">Brigade Sponsors</h4>
-         <p><a href='http://accela.com'><img src="images/accela.gif" width="300"></a></p>
-         <p><a href='http://sap.com'><img src="images/SAP.jpg" width="180" hspace="60"></a></p>
 
-      <h3>Free Stuff!</h3>
-      <h5>Just for Brigades with Captains</h5>
+      <h3>Free Stuff</h3>
+      <h5>For Official Brigade Chapters</h5>
       <ul>
           <li><a href="http://aws.amazon.com/">AWS Credits</a></li>
           <li><a href="http://digitalocean.com/">Digital Ocean Credits</a></li>
           <li><a href="http://www.justinmind.com/">JustInMind</a></li>
           <li><a href="https://www.mybalsamiq.com/">myBalsamiq</a></li>
+          <li><a href="https://www.twilio.com/">Twilio</a> - <a href="https://docs.google.com/document/d/1nPM9mAFMnhH7dozX51W9aPkKbANJczkseoQLNhmSBoU/edit?usp=sharing">How to use</a></li>
+          <!-- <li><a href="https://readme.io/">README.io</a></li>
+          <li><a href="https://office.com/">Office 365</a></li> -->
       </ul>
       <h5>For all Brigades</h5>
       <ul>
@@ -74,6 +77,9 @@
           <!-- <li><a href="#">OpenShift</a></li> -->
       </ul>
 
+      <h3>Brigade Sponsors</h3>
+      <p><a href='http://accela.com'><img src="images/accela.gif" width="300"></a></p>
+      <p><a href='http://sap.com'><img src="images/SAP.jpg" width="180" hspace="60"></a></p>
 
     </div>
   </div>
