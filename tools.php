58bea3046b6729a9d131590cb2cac1a46604a44e
Andrew Hyder <hyd415@gmail.com>
2014-12-09 15:03:46 -0800

    Replaced logo

--- tools.php
+++ tools.php
@@ -49,7 +49,7 @@
       </ul>
       <h3>Logos</h3>
       <ul class="list-ruled">
-          <li><a href='http://codeforamerica.org/media/images/logos/brigade.png'><img src='http://codeforamerica.org/media/images/logos/brigade.png' style='width: 270px; height: 102px;'></a></li>
+          <li><li><a href="images/new_logo.png"><img src="images/new_logo.png" /></a></li>
           <li><a href='http://codeforamerica.org/wp-content/uploads/2013/04/tumblr_glasses.png'><img height='100' src='http://codeforamerica.org/wp-content/uploads/2013/04/tumblr_glasses.png' width='100'></a></li>
           <li><a href='http://www.codeforamerica.org/wp-content/uploads/2013/02/the_brigade.png'><img height='100' src='http://www.codeforamerica.org/wp-content/uploads/2013/02/the_brigade.png' width='300'></a></li>
           <li><a href='http://codeforamerica.org/logos/'>More Code for America logos</a></li>
