394e5990c494c9d39713e084e53c9dbb90d3baa2
brielleplump <brielle@codeforamerica.org>
2014-12-10 11:29:41 -0800

    new strategic plan link

--- tools.php
+++ tools.php
@@ -21,7 +21,7 @@
           <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Zg2FDt7awgfCmdcbzMwKHMb1A7KDOhs_z7ibCb3TLLQ/edit'>How To: Anti-Harassment Policy</a></li>
           <li><a href='https://docs.google.com/document/d/1fR-uImfI3zbH__BhEzlsTgsKxgs21FK4C6zIwgeuWGw/edit?usp=sharing'>Websites, Meetups, and Social Media</a></li>
           <li><a href='https://docs.google.com/document/d/1r0aEcMwpMSwo4P-x6PJ5WV5y1ouciOkvRTiGATFJAmU/edit?usp=sharing'>Template: City Profile / City Assessment Document</a></li>
-          <li><a href='https://docs.google.com/document/d/1jTl0jRtyfPvlxweDuUSygnpo7ZG-rRV5lDnYpTLwbsI/edit?usp=sharing'>Template: Strategic Plan</a></li>
+          <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/16GO5lnwEBKMFhhvewhKiNUcvVbTw5NtunVxhmCS6vw0/edit#heading=h.yqfbsdykacwf'>Template: Strategic Plan</a></li>
 <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1RKkjW0YZfysBZ6JiJgV8j7o61H_Sh9y9pUl9fZzI2Bw/edit#heading=h.lfbjsp7i33tk'>Support Reimbursement Process</a></li>
       </ul>
 
