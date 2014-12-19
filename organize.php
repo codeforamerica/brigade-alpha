f51fc32e67c9d80ef0a32a40dafd05554e2265a1
Andrew Hyder <hyd415@gmail.com>
2014-12-19 11:58:01 -0800

    Revised organize page

--- organize.php
+++ organize.php
@@ -1,128 +1,82 @@
 <? include('top.php'); ?>
 
-<section>
+<section class="slab-black">
     <div class="layout-semibreve layout-centered">
-        <h2 class="isolate">Want to organize a Brigade?</h2>
-        <p>That's fantastic!  We are so excited you and your team are interested in joining this awesome network of developers, designers, community organizers, storytellers, government partners, and caring residents!
+      <figure>
+        <iframe width="560" height="315" style="padding:1px;border:1px solid black;" src="//www.youtube.com/embed/_9-E7CLQX48?list=PL65XgbSILalW_PV2Cgmz7gOWY3YLwC87M" frameborder="0" allowfullscreen></iframe>
+      </figure> 
     </div>
- </section>
-
- <section class="slab-black">
-		<div class="layout-semibreve layout-centered">
-			<figure>
-				<iframe width="560" height="315" style="padding:1px;border:1px solid black;" src="//www.youtube.com/embed/_9-E7CLQX48?list=PL65XgbSILalW_PV2Cgmz7gOWY3YLwC87M" frameborder="0" allowfullscreen></iframe>
-			</figure> 
-		</div>
-</section>
-
-<section>
-
-  <div class="slab-blue">
-    <div class="layout-semibreve layout-centered">
-      <h2 class="isolate">Ready to Get Started?</h2>
-      <p>Code for America Brigades organize through the <strong>4Ps: people, plans, partnerships, and participation</strong>. A group can join the Code for America Brigade network as a Brigade Network Member or an Official Brigade Chapter.</p>
-    </div>
-  </div>
-
-  <div class="layout-semibreve">
-    <div class="layout-minim">
-      <h3 class="isolate">Brigade Network Members</h3>
-      <p>Brigade Network Members have access to the Code for America Brigade network platforms Slack and the CFAPI, as well as toolkits and workshops. To access this level of support, your Brigade needs people and participation.</p>
-      <ol>
-        <li><strong>People</strong></li>
-          <ul>
-            <li>Host consistent hack nights (at least three)</li>
-            <li>Organize a <a href="https://docs.google.com/a/codeforamerica.org/document/d/1oEB1CuzCHldxP_bzcvmcrmK0yAE1BzV2AGhtwtYKZVY/edit">leadership team</a></li>
-            <li>Publish a <a href="https://github.com/codeforamerica/codeofconduct">Code of Conduct,</a> on your website</li>
-          </ul>
-        <li><strong>Participate</strong></li>
-          <ul>
-            <li>Share ideas on the Code for America Slack channel</li>
-            <li>Contibute content to the CFAPI</li>
-          </ul>
-      </ol>
-      <p>Once your ready to join, <a href="https://docs.google.com/a/codeforamerica.org/forms/d/1OZ_prvPITkPD1EzodwLVEzd4MSbBI0EjSb9hinuAzBE/viewform"><FONT COLOR="#399fd3">sign up</FONT></a> for the next Brigade Network Member onboarding session. 
-      We will host these calls once a month beginning January 2015.</p>
-      
-      <p>Once you’ve been onboarded, we’ll invite your leadership team to <strong></strong>participate</strong> in the Code for America Brigade Slack channel and the CFAPI.</p>
-     
-    </div>
-    <div class="layout-minim">
-      <h3 class="isolate"> Official Brigade Chapters</h3>
-      <p>Official Brigade Chapters have access to a full range of Code for America support, including financial resources. To access this level of support, your Brigade needs to organize around the 4Ps: people, plans, partnerships, participation. </FONT></a></p>
-      <ol>
-        <li><strong>People</strong></li>
-        <ul>
-          <li>Host consistent hack nights (at least three)</li>
-          <li>Organize <a href="https://docs.google.com/a/codeforamerica.org/document/d/1oEB1CuzCHldxP_bzcvmcrmK0yAE1BzV2AGhtwtYKZVY/edit">leadership team</a></li>
-          <li>Publish a <a href="https://github.com/codeforamerica/codeofconduct">Code of Conduct,</a> on your website</li>
-        </ul>
-        <li><strong>Plans</strong></li>
-        <ul>
-          <li>Create a plan that aligns with the <a href= "http://www.codeforamerica.org/governments/principles/">Code for America Digital Government Principles.</a></li>
-        </ul>
-        <li><strong>Partners</strong></li>
-        <ul>
-          <li>Collaborate with local government and local community groups and nonprofits.</li>
-        </ul>
-        <li><strong>Participate</strong></li>
-        <ul>
-          <li>Share ideas on the Code for America Slack channel</li>
-          <li>Contibute content to the CFAPI</li>
-        </ul>
-      </ol>
-      <p>Once your ready to join, <a href="https://docs.google.com/a/codeforamerica.org/forms/d/1odKnV_eFA2F7gix-AQXuTUhhIYrG0R6P1QYRoXw2EL8/viewform"><FONT COLOR="#399fd3">sign up</FONT></a> for the next Official Brigade Chapter onboarding session.  We will host these calls once a month beginning January 2015.</p>
-      <p>Once you’ve been onboarded, we’ll invite your leadership team to participate in the Code for America Brigade Slack channel and the CFAPI.</p>
-    </div>
-  </div>
 </section>
 
 <section class="slab-gray">
-  <div class="layout-breve">
-    <div class="layout-crotchet-staccato">
-      <h3 class="heading-contrast">For <strong>Citizens</strong></h3>
-
-      <div style="margin-left: 2em" class="list-ruled">
-        <ul>
-          <li><a href="http://brigade.codeforamerica.org/">Sign up</a> to join or start a civic hacking group.</li>
-
-          <li>Get in our <a href="https://github.com/codeforamerica">code</a> and hack.</li>
-
-          <li>Deploy and build apps in your city. Join a local <a href="http://brigade.codeforamerica.org/">CfA Brigade</a>.</li>
-
-          <li>Code with us!<a href="/geeks/civicissues">Check out the new Civic Tech Issue Finder.</a>.</li>
-        </ul>
-      </div>
+  <div class="layout-semibreve">
+    <div class="layout-centered">
+        <h2 class="isolate">Want to organize a Brigade?</h2>
+        <p>
+          That's fantastic!  We are so excited you and your 
+          team are interested in joining this awesome network 
+          of developers, designers, community organizers, 
+          storytellers, government partners, and caring residents.
+        </p>
     </div>
-
-    <div class="layout-crotchet-staccato">
-      <h3 class="heading-contrast">For <strong>Governments</strong></h3>
-      <ul class="list-ruled">
-        <li>Connect with your local <a href="http://brigade.codeforamerica.org">Brigade.</a></li>
-        <li>Release more <a href="http://codeforamerica.org/governments/capabilities/open-data/index.html">open data</a>.</li>
-        <li>Learn about <a href="http://wiki.civiccommons.org/Open_Data_Guidelines">open data best practices</a>.</li>
+    <form action="<?= $base_url ?>/signup" method="POST" accept-charset="UTF-8">
+      <ul class="list-form">
+        <li class="form-field">
+            <label for="name">Full name</label>
+            <input name="name" type="text" placeholder="Ben Franklin" required>
+        </li>
+        <li class="form-field">
+            <label for="email">Email</label>
+            <input name="email" type="text" placeholder="benfranklin@codeforamerica.org" required>
+        </li>
+        <li class="form-field">
+            <label for="location">City</label>
+            <input name="location" type="text" placeholder="Tulsa, OK">
+        </li>
+        <li class="form-field">
+            <label>
+              <input name="source" type="checkbox" value="organizer" checked/>
+              I want to lead a Brigade in my community!
+            </label>
+        </li>
+        <li class="form-field">
+            <input type="hidden" name="user[location_id]" value="" /><!-- why is this here? -->
+            <button>Join</button>
+        </li>
       </ul>
-    </div>
-
-    <div class="layout-crotchet-staccato">
-      <h3 class="heading-contrast">For <strong>Sponsors</strong></h3>
-      <ul class="list-ruled">
-        <li><a href="../../donate">Donate</a> to Support the CfA Brigade</li>
-        <li>To discuss a sizable donation to support the Brigade, please contact <a href="http://www.codeforamerica.org/people/ashley-meyers/">Ashley Meyers</a>.</li>
-        <li>To donate to a specific Brigade, please contact the <a href="http://codeforamerica.org/geeks/our-geeks/brigade-captains/">Brigade Captain</a>.</li>
-      </ul>
-    </div>
+    </form>
   </div>
 </section>
 
-<section>
-  <div class="layout-semibreve layout-centered">
-    <h4>Sponsors / Funders</h4>
-    <ul class="list-no-bullets layout-grid">
-      <li class="layout-minim"><a href="http://www.sap.com/index.html" target="_blank"><img src="images/SAP.jpg" height="80"></a></li>
-      <li class="layout-minim"><a href="http://www.accela.com/" target="_blank"><img src="images/accela.gif"></a></li>
+<section class="layout-semibreve">
+    <h2 class="layout-centered">Ready to Get Started?</h2>
+    <!-- <p>A group can join the Code for America Brigade network as a Brigade Network Member or an Official Brigade Chapter.</p> -->
+    <!-- <p>Code for America Brigades organize through the four Ps:</p> -->
+    <!-- <h4>people, plans, partnerships, and participation</h4> -->
+    <h3>People & Participation</h3>
+    <p>Your first step will be to get your <b>people</b> together. This includes:</p>
+    <ul>
+      <li>Organizing a core leadership team</li>
+      <li>Putting up a website that includes a Code of Conduct</li>
+      <li>Hosting three consistent hack nights</li>
     </ul>
-  </div>
+    <p>
+      Once you've done that, we'll invite you to <b>participate</b> in the 
+      Brigade Network channels where you'll be able to connect with 
+      other Brigades around the world to learn how they work and to 
+      share your experiences with them.
+    </p>
+    <h3>Plans & Partners</h3>
+    <p>To receive funding and additional resource and support, you'll need a <b>plan</b> and <b>partners</b>. This includes:</p>
+    <ul>
+      <li>Writing a strategic plan that maps to the Code for America <a href="http://www.codeforamerica.org/governments/principles/">Principles</a></li>
+      <li>Building partnerships with community groups and local government</li>
+    </ul>
+
+    <p>
+      Joining from outside the United States? Find out more at 
+      <a href="http://code-for-all.github.io/codeforall.org/">codeforall.org</a>.
+    </p>
 </section>
 
 <? include('bottom.php') ?>
