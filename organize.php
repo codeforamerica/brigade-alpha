9394918e18221101f2061c981fb110438ac5690b
Andrew Hyder <hyd415@gmail.com>
2014-12-30 09:44:22 -0800

    Organize updates

--- organize.php
+++ organize.php
@@ -9,16 +9,17 @@
 </section>
 
 <section class="slab-gray">
+
   <div class="layout-semibreve">
+
     <div class="layout-centered">
         <h2 class="isolate">Want to organize a Brigade?</h2>
-        <p>
-          That's fantastic!  We are so excited you and your 
+        <p>That's fantastic!  We are so excited you and your 
           team are interested in joining this awesome network 
           of developers, designers, community organizers, 
-          storytellers, government partners, and caring residents.
-        </p>
+          storytellers, government partners, and caring residents.</p>
     </div>
+
     <form action="<?= $base_url ?>/signup" method="POST" accept-charset="UTF-8">
       <ul class="list-form">
         <li class="form-field">
@@ -45,15 +46,16 @@
         </li>
       </ul>
     </form>
+
   </div>
+
 </section>
 
 <section class="layout-semibreve">
+    
     <h2 class="layout-centered">Ready to Get Started?</h2>
-    <!-- <p>A group can join the Code for America Brigade network as a Brigade Network Member or an Official Brigade Chapter.</p> -->
-    <!-- <p>Code for America Brigades organize through the four Ps:</p> -->
-    <!-- <h4>people, plans, partnerships, and participation</h4> -->
-    A group can join the Code for America Brigade network as a <b>Brigade Network Member</b> or an <b>Official Brigade Chapter</b>. 
+    <p>A group can join the Code for America Brigade network as a <b>Brigade Network Member</b> or an <b>Official Brigade Chapter</b>. </p>
+    
     <h3>People & Participation</h3>
     <p>Your first step will be to get your <b>people</b> together. This includes:</p>
     <ul>
@@ -67,24 +69,22 @@
       other Brigades around the world through our various digitial channels to learn how they work and to 
       share your experiences with them.
     </p>
+    
     <h3>Plans & Partners</h3>
     <p>To receive funding and additional resource and support, you'll need get your people in order, participate in the network, AND establish a <b>plan</b> and <b>partners</b>. This includes:</p>
     <ul>
       <li>Writing a strategic plan that maps to the Code for America <a href="http://www.codeforamerica.org/governments/principles/">Principles</a></li>
       <li>Building partnerships with community groups and local government</li>
     </ul>
-     <p>
-      Once you've done that, we'll invite you to join us as an 
+     <p>Once you've done that, we'll invite you to join us as an 
       <b>Official Brigade Chapter</b>. You'll be able to connect with 
-      other Brigades just as a Network Member does, plus recieve fincial and organizing resources.  
-    </p>
-    <p>
-    <b>To learn more about becoming either a Brigade Network Member or an Official Brigade Chapter fill out the above form and we will send you all the details!</b>
-    </p>
-    <p>
-     <b> Joining from outside the United States?</b> Find out more at 
-      <a href="http://code-for-all.github.io/codeforall.org/">codeforall.org</a>.
-    </p>
+      other Brigades just as a Network Member does, plus recieve fincial and organizing resources.</p>
+
+    <p><b>To learn more about becoming either a Brigade Network Member or an Official Brigade Chapter fill out the above form and we will send you all the details!</b></p>
+
+    <p><b> Joining from outside the United States?</b> Find out more at 
+      <a href="http://code-for-all.github.io/codeforall.org/">codeforall.org</a>.</p>
+
 </section>
 
 <? include('bottom.php') ?>
