02adeba0d3338d798d9ea374b31f9b8048831398
brielleplump <brielle@codeforamerica.org>
2015-01-06 12:18:09 -0800

    edits

--- thank-you.php
+++ thank-you.php
@@ -56,22 +56,22 @@
     Once you've done that, <a href="https://docs.google.com/a/codeforamerica.org/forms/d/1UIcMpqI4eGeFizGaViPC6odaGGIe59pvOtWhToA6xtY/viewform">fill out this form</a> and we'll set up some time to chat and invite you to participate as a <b> Brigade Network Member </b>. You'll be able to connect with other Brigades around the world through our various digitial channels to learn how they work and to share your experiences with them.
     </p>
 
-    <p>Want access to funding and additional resources? Whenever you are ready you can add plans and partnerships to your Brigade. </p>
+    <p>Want access to funding and additional resources? Whenever you are ready you can add plans and partnerships to your Brigade and join as an Official Chapter.  This includes: </p>
     <ul>
         <li>Writing a <a href="https://docs.google.com/a/codeforamerica.org/document/d/16GO5lnwEBKMFhhvewhKiNUcvVbTw5NtunVxhmCS6vw0/edit">strategic plan</a> that maps to the <a href="http://www.codeforamerica.org/governments/principles/">Code for America Principles</a></li>
-        <li>Building partnerships with other community groups and local government, (<a href="http://www.codeforamerica.org/blog/2014/10/13/want-to-take-your-brigade-to-the-next-level-develop-it/">click here</a> to read about a community partner relationship with Code for Philly, and <a href="http://code4sac.org/were-coding-for-california/">click here</a> to learn how Code for Sacramento is working with one of their local government partners, the California Department of Public Health (CDPH).</li>
+        <li>Building partnerships with other community groups and local government, (<a href="http://www.codeforamerica.org/blog/2014/10/13/want-to-take-your-brigade-to-the-next-level-develop-it/">click here</a> to read about a community partner relationship with Code for Philly, and <a href="http://code4sac.org/were-coding-for-california/">click here</a> to learn how Code for Sacramento is working with one of their local government partners.</li>
     </ul>
     
      <p>
-    Once you've done that, <a href="https://docs.google.com/a/codeforamerica.org/forms/d/1odKnV_eFA2F7gix-AQXuTUhhIYrG0R6P1QYRoXw2EL8/viewform">fill out this form</a> and we'll set up some time to chat and invite you to participate as an <b> Official Brigade Chapter</b>. I.E. You can join as an Official Brigade Chapter after you have participated as a Brigade Network Member for a while, or by submitting to the Official Brigade Chapter form with all four Ps in one swoop). 
+    Once you've done that, <a href="https://docs.google.com/a/codeforamerica.org/forms/d/1odKnV_eFA2F7gix-AQXuTUhhIYrG0R6P1QYRoXw2EL8/viewform">fill out this form</a> and we'll set up some time to chat and invite you to participate as an <b> Official Brigade Chapter</b>. I.E. You can join as an Official Brigade Chapter after you have participated as a Brigade Network Member for a while, or in one swoop by submitting to the Official Brigade Chapter form once you have completed all four Ps). 
     </p>
     
-    <p>If you are joining from outside the United States, you can participate in the Brigade network as an Brigade Network Member. If your Brigade grows, we may want to consider becoming a <b> Code for All </b> member, which is the support route interntaional Brigades take instead of becoming Official Brigade Chapters.   Find out more at <a href="codeforall.org">codeforall.org</a>.</p>
+    <p><b>If you are joining from outside the United States</b>, you can participate in the Brigade network as an Brigade Network Member. If your Brigade grows, we may want to consider becoming a Code for All member, which is the support route interntaional Brigades take instead of becoming Official Brigade Chapters.   Find out more at <a href="codeforall.org">codeforall.org</a>.</p>
 
-    <p>Questions? Hit up Communities Coordinator, Brielle Plump, at brielle@codeforamerica.org</p>
+    <p>Questions? Check out this <a href="https://docs.google.com/a/codeforamerica.org/document/d/1b4pxoRxm8RMDfPhkqBHk9TT9uRHJ-jPDbJpBSW9nvz0/edit">FAQ doc</a>. You can sign up for Google Hangout office hours from the FAQ, or reach out to Communities Coordinator, Brielle Plump, at brielle@codeforamerica.org.</p>
 
-    <p>Thank you for volunteering to make your community stronger,
-    Hannah, Brielle, Preston, Andrew, and Catherine
+    <p><h3>Thank you for volunteering to make your community stronger!</h3> <br><br>
+    Hannah, Brielle, Preston, Andrew, and Catherine <br>
     Code for America Communities Team</p>
 
   </div>
@@ -80,14 +80,13 @@
 <? } else { ?>
 
   <div id="no_brigade_text">
-    <p><b>Thanks for your interest in the Code for America Brigade community.</b></p>
+    <p><b><h3>Thank you for your interest in the Code for America Brigade community!</h3></b></p>
     <p>We will add you to our mailing list so you can keep stay up to date with both local opportunities and national events.</p>
-    <p>If you want to reconsider leading a Brigade in your city,  
-    <p><a href='https://www.youtube.com/watch?v=f1oBpGQbdmg'>Click here to view the "How to start a Brigade" video, then re-submit your name into the form on <a href="codeforamerica.org/brigade">on our website and click "I want to lead a Brigade in my community."</a></a></p>
-    <p>Good luck and we'll be in touch.</p>
-    <br>
-    <p><i>Brielle Plump - Communities Coordinator</i></p>
-    <p><a href='mailto:brielle@codeforamerica.org'>brielle@codeforamerica.org</a></p>
+    <p>If you want to reconsider leading a Brigade in your city,<a href='https://www.youtube.com/watch?v=f1oBpGQbdmg'>click here to view the "How to start a Brigade" video</a>, then re-submit your name into the form on <a href="codeforamerica.org/brigade">on our website </a> and click "I want to lead a Brigade in my community."</a></p>
+    <p>We'll be in touch, but feel free to reach out with your ideas and questions anytime.</p>
+    <p>Sincerley,<br>
+    Brielle Plump - Communities Coordinator<br>
+    <a href='mailto:brielle@codeforamerica.org'>brielle@codeforamerica.org</a></p>
   </div>
 
 <? } ?>
