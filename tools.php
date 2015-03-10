c5709f4f6099e7ab546bf0e07c4fd568861282b3
brielleplump <brielle@codeforamerica.org>
2015-03-10 15:01:31 -0700

    total re-order

--- tools.php
+++ tools.php
@@ -8,46 +8,51 @@
 <section>
   <div class="layout-breve">
     <div class="layout-crotchet-staccato">
-      <h3><strong>Getting started</strong></h3>
+      <h3><strong>Organizing Resources</strong></h3>
+      <h3>People</h3>
       <ul class="list-ruled">
-
-          <li><a href='https://github.com/codeforamerica/codeofconduct'>Code for America's Code of Conduct</a></li>
-          <li><a href='https://docs.google.com/document/d/1oEB1CuzCHldxP_bzcvmcrmK0yAE1BzV2AGhtwtYKZVY/edit?usp=sharing'>Roles within a Local Brigade</a></li>
-          <li><a href='https://docs.google.com/document/d/1ukivPQNeIH2GcJHcZ50yjzh1uQj70FlilFQcUKOH49k/edit?usp=sharing'>How To: Host a Hack Night</a></li>
-          <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Zg2FDt7awgfCmdcbzMwKHMb1A7KDOhs_z7ibCb3TLLQ/edit'>How To: Anti-Harassment Policy</a></li>
-          <li><a href='https://docs.google.com/document/d/1WtoE_Kfqcu73MCUOIXujrzTfqrQAW3DGt0sC8iKJ6Rw/edit?usp=sharing'>How To: Collaborate with Government</a></li>
-          <li><a href='https://docs.google.com/document/d/1LIUcCLwmn-SR6_X93TOMIoIJOhNXd0t_27TAIOofdIo/edit?usp=sharing'>How To: Brand and Market Your Brigade</a></li>
-          <li><a href='https://docs.google.com/document/d/1AuMsUqCZ1Em99NvQje1dCI2cD1C54dD711sbm6FgYhE/edit?usp=sharing'>How To: Welcome New Members</a></li>
-           <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1wo_skwazuom_Zkp7AmCSjy5JD8Z5WYSbhr2IUmqWdQ0/edit'>Projects for new Brigade members</a></li>
-          <li><a href='https://docs.google.com/document/d/1fR-uImfI3zbH__BhEzlsTgsKxgs21FK4C6zIwgeuWGw/edit?usp=sharing'>Websites, Meetups, and Social Media</a></li>
-          <li><a href='https://docs.google.com/document/d/1r0aEcMwpMSwo4P-x6PJ5WV5y1ouciOkvRTiGATFJAmU/edit?usp=sharing'>Template: City Profile / City Assessment Document</a></li>
-          <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/16GO5lnwEBKMFhhvewhKiNUcvVbTw5NtunVxhmCS6vw0/edit#heading=h.yqfbsdykacwf'>Template: Strategic Plan (Offical Brigades)</a></li>
-          <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Uc8cmQt9-c4DaMD0hvamQwG6cOxNtjFO5Mq6dcdQ9_o/edit'>How To: Local Fundraising</a></li>
-          <li><a href='https://docs.google.com/a/codeforamerica.org/forms/d/1smbubzz4J-L_KuRt4CTbVIzxf9vvG7ts7gNAbM01Lqw/viewform'>Brigade Support Request Form</a></li>  
-<li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1RKkjW0YZfysBZ6JiJgV8j7o61H_Sh9y9pUl9fZzI2Bw/edit#heading=h.lfbjsp7i33tk'>Support Reimbursement Process</a></li>
-      </ul>
-
-      <h3>Events</h3>
-      <ul class="list-ruled">
-          <li><a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.indd'>Name Tag Template</a> <a href='http://dl.dropbox.com/u/85457263/brigade_name_tag.pdf'>[PDF version]</a></li>
-          <li><a href='https://docs.google.com/open?id=0B_WC8QOgiwjzT05IdVdwSk5TX0E'>Meetup Sign</a></li>
-      </ul>
+        <li><a href='https://docs.google.com/document/d/1oEB1CuzCHldxP_bzcvmcrmK0yAE1BzV2AGhtwtYKZVY/edit?usp=sharing'>Roles within a Local Brigade</a></li>
+        <li><a href='https://github.com/codeforamerica/codeofconduct'>Code for America's Code of Conduct</a></li>
+        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Zg2FDt7awgfCmdcbzMwKHMb1A7KDOhs_z7ibCb3TLLQ/edit'>Code of Conduct and Anti-Harassment Policy Guide</a></li>
+        <li><a href='https://docs.google.com/document/d/1LIUcCLwmn-SR6_X93TOMIoIJOhNXd0t_27TAIOofdIo/edit?usp=sharing'>Branding Guidelines</a></li>
+        <li><a href='https://docs.google.com/document/d/1OY0s8BJR9A2aW1ivlNACTbi0dWOh9njYpHp53G1shFs/edit'>Brigade Overview</a></li>
+        <li><a href='https://docs.google.com/presentation/d/1MSyVMauTv4HcPcgXmF2l-Afzjw35UfieCj5mKDK7fvY/edit?usp=sharing'>Sample Intro Slide Deck</a></li>
+        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1wo_skwazuom_Zkp7AmCSjy5JD8Z5WYSbhr2IUmqWdQ0/edit'>Projects for new Brigade members</a></li>
+        <li><a href="http://www.codeforamerica.org/brigade/checkin/">Event check in tool</a> - <a href="https://github.com/codeforamerica/brigade-alpha/tree/master/checkin#brigade-checkins">More info</a></li>
+     </ul>
 
     </div>
 
     <div class="layout-crotchet-staccato">
-      <h3>For Your Website</h3>
+    <h3>Plans</h3>
       <ul class="list-ruled">
-        <li><a href="http://www.codeforamerica.org/brigade/checkin/">An event check in tool</a> - <a href="https://github.com/codeforamerica/brigade-alpha/tree/master/checkin#brigade-checkins">More info</a></li>
-        <li><a href="http://www.codeforamerica.org/geeks/civicissues/embed">Embed your Brigade's GitHub Issues</a></li>
-      </ul>
-      <h3>Marketing</h3>
+    <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/16GO5lnwEBKMFhhvewhKiNUcvVbTw5NtunVxhmCS6vw0/edit#heading=h.yqfbsdykacwf'>Template: 2015 Strategic Plan a></li>
+    <li><a href='https://docs.google.com/document/d/1r0aEcMwpMSwo4P-x6PJ5WV5y1ouciOkvRTiGATFJAmU/edit?usp=sharing'>Template: City Profile </a></li>
+     
+      </ul> 
+      
+    <h3>Partnerships</h3>
       <ul class="list-ruled">
-          <li><a href='https://docs.google.com/document/d/1LIUcCLwmn-SR6_X93TOMIoIJOhNXd0t_27TAIOofdIo/edit?usp=sharing'>Branding Guidelines</a></li>
-          <li><a href='https://docs.google.com/document/d/1OY0s8BJR9A2aW1ivlNACTbi0dWOh9njYpHp53G1shFs/edit'>One Page Flyer</a></li>
-          <li><a href='https://docs.google.com/presentation/d/1MSyVMauTv4HcPcgXmF2l-Afzjw35UfieCj5mKDK7fvY/edit?usp=sharing'>Slide Deck</a></li>
-          
+    <li><a href='https://docs.google.com/document/d/1WtoE_Kfqcu73MCUOIXujrzTfqrQAW3DGt0sC8iKJ6Rw/edit?usp=sharing'>10 Ways to  Collaborate with Government</a></li>
+    <li><a href= https://docs.google.com/a/codeforamerica.org/document/d/1HtwcxiARA57TFI3wpeIMfE27adTa7TTJG-I12XepMLU/edit >Build a Community Partnership (Girl Develop It) </a></li>
+  
       </ul>
+      
+    <h3>Participation</h3>
+    <ul class="list-ruled">
+    <li><a href="http://forum.codeforamerica.org/">Brigade Forum</a></li>
+    <li><a href="http://www.codeforamerica.org/geeks/civicissues/embed">Add Projects to the Civic Issue Finder</a></li>
+
+      </ul>
+      
+    <h3><strong>Financial Resources</strong></h3>
+      <ul class="list-ruled">
+        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Uc8cmQt9-c4DaMD0hvamQwG6cOxNtjFO5Mq6dcdQ9_o/edit'> Local Fundraising Guide</a></li>
+        <li><a href='https://docs.google.com/a/codeforamerica.org/forms/d/1smbubzz4J-L_KuRt4CTbVIzxf9vvG7ts7gNAbM01Lqw/viewform'>Brigade Support Request Form</a></li>  
+        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1RKkjW0YZfysBZ6JiJgV8j7o61H_Sh9y9pUl9fZzI2Bw/edit#heading=h.lfbjsp7i33tk'>Support Reimbursement Process</a></li>
+  
+      </ul>
+
       <h3>Logos</h3>
       <ul class="list-ruled">
           <li><li><a href="images/new_logo.png"><img src="images/new_logo.png" /></a></li>
@@ -57,8 +62,8 @@
     </div>
     <div class="layout-crotchet-staccato">
 
-      <h3>Free Stuff</h3>
-      <h5>For Official Brigade Chapters</h5>
+      <h3><strong>Free Tools</strong></h3>
+      <h3>For Official Brigade Chapters</h3>
       <ul>
           <li><a href="http://aws.amazon.com/">AWS Credits</a></li>
           <li><a href="http://digitalocean.com/">Digital Ocean Credits</a></li>
@@ -69,7 +74,7 @@
           <li><a href="https://office.com/">Microsoft Office 365</a> - <a href="http://forum.codeforamerica.org/t/free-email-for-your-brigade-google-apps-and-office-365/172">Forum post</a></li>
           <li><a href="http://www.google.com/nonprofits/">Google Apps for Nonprofits</a> - <a href="http://forum.codeforamerica.org/t/free-email-for-your-brigade-google-apps-and-office-365/172">Forum post</a></li>
       </ul>
-      <h5>For all Brigades</h5>
+      <h3>For all Brigades</h3>
       <ul>
           <li><a href="https://brigades.opendatanetwork.com/">Socrata Open Data Portals</a></li>
           <li><a href="https://cloud.google.com/developers/starterpack/">Google Cloud Platform</a></li>
