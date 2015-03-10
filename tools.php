1c73d8d2f13444c6e7976b3fe0e1c87c3a8e1569
Andrew Hyder <hyd415@gmail.com>
2015-03-10 15:37:10 -0700

    Updates to toolkit page

--- tools.php
+++ tools.php
@@ -5,10 +5,25 @@
     $base_url = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
 
 ?>
+
 <section>
   <div class="layout-breve">
     <div class="layout-crotchet-staccato">
       <h3><strong>Organizing Resources</strong></h3>
+    </div>
+    <div class="layout-crotchet-staccato">
+      <h3><strong>Other Resources</strong></h3>
+    </div>
+    <div class="layout-crotchet-staccato">
+      <h3><strong>Free Tools</strong></h3>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="layout-breve">
+    <div class="layout-crotchet-staccato">
+
       <h3>People</h3>
       <ul class="list-ruled">
         <li><a href='https://docs.google.com/document/d/1oEB1CuzCHldxP_bzcvmcrmK0yAE1BzV2AGhtwtYKZVY/edit?usp=sharing'>Roles within a Local Brigade</a></li>
@@ -21,73 +36,71 @@
         <li><a href="http://www.codeforamerica.org/brigade/checkin/">Event check in tool</a> - <a href="https://github.com/codeforamerica/brigade-alpha/tree/master/checkin#brigade-checkins">More info</a></li>
      </ul>
 
+      <h3>Plans</h3>
+      <ul class="list-ruled">
+        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/16GO5lnwEBKMFhhvewhKiNUcvVbTw5NtunVxhmCS6vw0/edit#heading=h.yqfbsdykacwf'>Template: 2015 Strategic Plan</a></li>
+        <li><a href='https://docs.google.com/document/d/1r0aEcMwpMSwo4P-x6PJ5WV5y1ouciOkvRTiGATFJAmU/edit?usp=sharing'>Template: City Profile </a></li>
+      </ul>
+
+      <h3>Partnerships</h3>
+        <ul class="list-ruled">
+      <li><a href='https://docs.google.com/document/d/1WtoE_Kfqcu73MCUOIXujrzTfqrQAW3DGt0sC8iKJ6Rw/edit?usp=sharing'>10 Ways to  Collaborate with Government</a></li>
+      <li><a href= https://docs.google.com/a/codeforamerica.org/document/d/1HtwcxiARA57TFI3wpeIMfE27adTa7TTJG-I12XepMLU/edit >Build Community Partnerships <br/> (Girl Develop It) </a></li>
+
+        </ul>
+
+      <h3>Participation</h3>
+      <ul class="list-ruled">
+      <li><a href="http://forum.codeforamerica.org/">Brigade Forum</a></li>
+      <li><a href="http://www.codeforamerica.org/geeks/civicissues/embed">Add Projects to the Civic Issue Finder</a></li>
+
+        </ul>
+
     </div>
 
     <div class="layout-crotchet-staccato">
-    <h3>Plans</h3>
-      <ul class="list-ruled">
-    <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/16GO5lnwEBKMFhhvewhKiNUcvVbTw5NtunVxhmCS6vw0/edit#heading=h.yqfbsdykacwf'>Template: 2015 Strategic Plan a></li>
-    <li><a href='https://docs.google.com/document/d/1r0aEcMwpMSwo4P-x6PJ5WV5y1ouciOkvRTiGATFJAmU/edit?usp=sharing'>Template: City Profile </a></li>
-     
-      </ul> 
-      
-    <h3>Partnerships</h3>
-      <ul class="list-ruled">
-    <li><a href='https://docs.google.com/document/d/1WtoE_Kfqcu73MCUOIXujrzTfqrQAW3DGt0sC8iKJ6Rw/edit?usp=sharing'>10 Ways to  Collaborate with Government</a></li>
-    <li><a href= https://docs.google.com/a/codeforamerica.org/document/d/1HtwcxiARA57TFI3wpeIMfE27adTa7TTJG-I12XepMLU/edit >Build a Community Partnership (Girl Develop It) </a></li>
-  
-      </ul>
-      
-    <h3>Participation</h3>
-    <ul class="list-ruled">
-    <li><a href="http://forum.codeforamerica.org/">Brigade Forum</a></li>
-    <li><a href="http://www.codeforamerica.org/geeks/civicissues/embed">Add Projects to the Civic Issue Finder</a></li>
+      <h3>Financial Resources</h3>
+        <ul class="list-ruled">
+          <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Uc8cmQt9-c4DaMD0hvamQwG6cOxNtjFO5Mq6dcdQ9_o/edit'> Local Fundraising Guide</a></li>
+          <li><a href='https://docs.google.com/a/codeforamerica.org/forms/d/1smbubzz4J-L_KuRt4CTbVIzxf9vvG7ts7gNAbM01Lqw/viewform'>Brigade Support Request Form</a></li>
+          <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1RKkjW0YZfysBZ6JiJgV8j7o61H_Sh9y9pUl9fZzI2Bw/edit#heading=h.lfbjsp7i33tk'>Support Reimbursement Process</a></li>
 
-      </ul>
-      
-    <h3><strong>Financial Resources</strong></h3>
-      <ul class="list-ruled">
-        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1Uc8cmQt9-c4DaMD0hvamQwG6cOxNtjFO5Mq6dcdQ9_o/edit'> Local Fundraising Guide</a></li>
-        <li><a href='https://docs.google.com/a/codeforamerica.org/forms/d/1smbubzz4J-L_KuRt4CTbVIzxf9vvG7ts7gNAbM01Lqw/viewform'>Brigade Support Request Form</a></li>  
-        <li><a href='https://docs.google.com/a/codeforamerica.org/document/d/1RKkjW0YZfysBZ6JiJgV8j7o61H_Sh9y9pUl9fZzI2Bw/edit#heading=h.lfbjsp7i33tk'>Support Reimbursement Process</a></li>
-  
-      </ul>
+        </ul>
 
-      <h3>Logos</h3>
-      <ul class="list-ruled">
-          <li><li><a href="images/new_logo.png"><img src="images/new_logo.png" /></a></li>
-          <li><a href='http://codeforamerica.org/logos/'>More Code for America logos</a></li>
-      </ul>
+        <h3>Logos</h3>
+        <ul class="list-ruled">
+            <li><li><a href="images/new_logo.png"><img src="images/new_logo.png" /></a></li>
+            <li><a href='http://codeforamerica.org/logos/'>More Code for America logos</a></li>
+        </ul>
 
     </div>
     <div class="layout-crotchet-staccato">
 
-      <h3><strong>Free Tools</strong></h3>
-      <h3>For Official Brigade Chapters</h3>
-      <ul>
-          <li><a href="http://aws.amazon.com/">AWS Credits</a></li>
-          <li><a href="http://digitalocean.com/">Digital Ocean Credits</a></li>
-          <li><a href="http://www.justinmind.com/">JustInMind</a></li>
-          <li><a href="https://www.mybalsamiq.com/">myBalsamiq</a></li>
-          <li><a href="https://www.twilio.com/">Twilio</a> - <a href="https://docs.google.com/document/d/1nPM9mAFMnhH7dozX51W9aPkKbANJczkseoQLNhmSBoU/edit?usp=sharing">How to use</a></li>
-          <li><a href="https://www.usertesting.com/">UserTesting.com</a> - <a href="http://forum.codeforamerica.org/t/free-usertesting-com-videos-for-brigades/161">Forum post</a>
-          <li><a href="https://office.com/">Microsoft Office 365</a> - <a href="http://forum.codeforamerica.org/t/free-email-for-your-brigade-google-apps-and-office-365/172">Forum post</a></li>
-          <li><a href="http://www.google.com/nonprofits/">Google Apps for Nonprofits</a> - <a href="http://forum.codeforamerica.org/t/free-email-for-your-brigade-google-apps-and-office-365/172">Forum post</a></li>
-      </ul>
-      <h3>For all Brigades</h3>
-      <ul>
-          <li><a href="https://brigades.opendatanetwork.com/">Socrata Open Data Portals</a></li>
-          <li><a href="https://cloud.google.com/developers/starterpack/">Google Cloud Platform</a></li>
-          <li><a href="http://www.civicdata.com/">CivicData.com</a></li>
-          <li><a href="https://readme.io/">README.io</a> - <a href="http://forum.codeforamerica.org/t/free-readme-io-donation-for-brigade-projects">Forum post</a></li>
-          <li><a href="https://www.flowdock.com/">Flowdock</a></li>
-          <!-- <li><a href="#">OpenShift</a></li> -->
-      </ul>
+        <h3>For Official Brigade Chapters</h3>
+        <ul>
+            <li><a href="http://aws.amazon.com/">AWS Credits</a></li>
+            <li><a href="http://digitalocean.com/">Digital Ocean Credits</a></li>
+            <li><a href="http://www.justinmind.com/">JustInMind</a></li>
+            <li><a href="https://www.mybalsamiq.com/">myBalsamiq</a></li>
+            <li><a href="https://www.twilio.com/">Twilio</a> - <a href="https://docs.google.com/document/d/1nPM9mAFMnhH7dozX51W9aPkKbANJczkseoQLNhmSBoU/edit?usp=sharing">How to use</a></li>
+            <li><a href="https://www.usertesting.com/">UserTesting.com</a> - <a href="http://forum.codeforamerica.org/t/free-usertesting-com-videos-for-brigades/161">Forum post</a>
+            <li><a href="https://office.com/">Microsoft Office 365</a> - <a href="http://forum.codeforamerica.org/t/free-email-for-your-brigade-google-apps-and-office-365/172">Forum post</a></li>
+            <li><a href="http://www.google.com/nonprofits/">Google Apps for Nonprofits</a> - <a href="http://forum.codeforamerica.org/t/free-email-for-your-brigade-google-apps-and-office-365/172">Forum post</a></li>
+        </ul>
+        <h3>For all Brigades</h3>
+        <ul>
+            <li><a href="https://brigades.opendatanetwork.com/">Socrata Open Data Portals</a></li>
+            <li><a href="https://cloud.google.com/developers/starterpack/">Google Cloud Platform</a></li>
+            <li><a href="http://www.civicdata.com/">CivicData.com</a></li>
+            <li><a href="https://readme.io/">README.io</a> - <a href="http://forum.codeforamerica.org/t/free-readme-io-donation-for-brigade-projects">Forum post</a></li>
+            <li><a href="https://www.flowdock.com/">Flowdock</a></li>
+            <!-- <li><a href="#">OpenShift</a></li> -->
+        </ul>
 
-      <h3>Funders</h3>
-      <p><a href="https://www.rallydev.com/about/rally-for-impact"><img src="images/rally.png" height="100px"></a></p>
-
+        <h3>Funders</h3>
+        <p><a href="https://www.rallydev.com/about/rally-for-impact"><img src="images/rally.png" height="100px"></a></p>
     </div>
   </div>
 </section>
+
 <? include('bottom.php') ?>
