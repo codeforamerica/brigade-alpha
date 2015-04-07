f18721ef2bb06daa206ede6f13f44bf02b937370
Preston Rhea <preston@codeforamerica.org>
2015-04-07 15:23:32 -0700

    fixed list display size + video link
    Still have a nested list.

--- tools/recruiting-diverse-leadership.php
+++ tools/recruiting-diverse-leadership.php
@@ -6,7 +6,7 @@
 
     <h1>Recruiting for Diverse Leadership</h1>
 
- <iframe width="560" height="315" src="//www.youtube.com/embed/_9-E7CLQX48?list=PL65XgbSILalW_PV2Cgmz7gOWY3YLwC87M" frameborder="0" allowfullscreen></iframe>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/cVY0L4_BPrE" frameborder="0" allowfullscreen></iframe>
 
 <p>Check out the conversation we had with Spike and Ellie to learn about how they recruited for diverse leadership when building out their executive team.</p>
 
@@ -23,9 +23,9 @@
     <p>Check out the live notes for the training <a href="http://forum.codeforamerica.org/t/12-march-2015-brigade-training-recruiting-for-diverse-leadership-with-open-oakland/177">here.</a> This training also had a longer series of crowdsourced notes which you can find <a href="https://docs.google.com/a/codeforamerica.org/document/d/1G12KAH4lXrc6dwuq3hK7t2RFoRLfZ1gLYEdmFQCVqVg/edit">here.</a></p>
     
     <p>A few key points from the 47-minute discussion:</p>
-    <h2>
+    <h4>
       <ol>
-        <li>Oakland is <a href="http://priceonomics.com/the-most-and-least-diverse-cities-in-america/">by many measures the most diverse city in the United States</a>, but Open Oakland did not meet that standard. Open Oakland tried to build an organization that could change the city and build trust.</li>
+        <li>Oakland is <a href="http://priceonomics.com/the-most-and-least-diverse-cities-in-america/">by many measures the most diverse city in the United States</a>, but Open Oakland found that they did not meet that standard for representation. Open Oakland tried to build an organization that could change the city and build trust.</li>
         <li>Open Oakland's leadership picked 17 people in total to discuss the future of the organization. Their recommended process for a leadership change:
           <ol>
             <li>Curate a few folks who are regularly attending and see whether they are interested in being a leader or helping to craft a leadership structure.</li>
@@ -33,6 +33,7 @@
             <li>Put in place an action plan.</li>
             <li>Note that leadership roles aren't about recognition - they are about hard work and an opportunity to do more, think bigger, and work harder for the organization.</li>
 </li>
+          </ol>
         <li>Open Oakland developed a "SWAT" team to identify roles and people to fill them that would represent Oakland's community. They first identified functional areas, then added roles to cover that work, like partnerships, outreach, development. <a href="http://openoakland.org/jobs/">Visit Open Oakland's website</a> for position descriptions.</li>
         <li>Open Oakland engaged some seed funding from the Kapor Foundation to get <a href="https://twitter.com/nmsanchez">Nicole Sanchez</a> from Vaya Consulting to help them develop a process to recruit people.</li>
         <li>They did a lot of broad outreach to find new voices. The relationships among existing Open Oakland volunteers were already close, and they did not want the process to only engage their close associations as this would not help them reach more of Oakland. They used anonymous applications as much as possible, and couldn’t see each other’s hand-written notes or speculations or analysis. Nicole explained this controlled the process, kept our selection based on gut feedback, catalyzed by people’s observations which was a fair assessment. They didn’t know demographic info until they had ranked the roles. The leadership team only analyzed diversity after they had picked the best qualified folks [on paper] for the job. This process resulted in 11 new leaders, a majority of them people of color, several of them queer. However, Open Oakland contends they could have done better with gender diversity.</li>
@@ -42,7 +43,7 @@
           <li>SPIKE: Diversity isn’t an idea, it’s an action. It was necessary to do a lot of things to work on this. It was a hard thing to do, but it’s important for us to build a strong base as we get more serious about Open Oakland.</li>
         </li>
       </ol>
-    </h2>
+    </h4>
 
   </div>
 
@@ -62,15 +63,6 @@
     </h2>
 
     </div>
-
-</section>
-
-<section class="slab-blue">
-
-  <div class="layout-semibreve">
-    <h2>Another great point</h2>
-    <p>Drinking vinegar kogi tattooed food truck. Thundercats heirloom irony tousled kitsch Blue Bottle. Blog ugh you probably haven't heard of them chambray, distillery typewriter Marfa 3 wolf moon swag shabby chic cronut narwhal. Aesthetic pug cliche, McSweeney's irony four loko four dollar toast Helvetica next level selfies seitan street art pork belly gentrify listicle. Kogi roof party leggings, Shoreditch cardigan four dollar toast Etsy tousled butcher actually American Apparel selfies occupy food truck pickled. Ennui chambray occupy organic aesthetic art party. Crucifix irony keytar, XOXO dreamcatcher direct trade iPhone swag Odd Future Banksy.</p>
-  </div>
 
 </section>
 
