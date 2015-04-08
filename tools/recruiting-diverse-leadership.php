de6a54124709627f79da39dce7624f8659c36a87
Preston Rhea <preston@codeforamerica.org>
2015-04-08 15:15:59 -0700

    minor text and ordering edits

--- tools/recruiting-diverse-leadership.php
+++ tools/recruiting-diverse-leadership.php
@@ -25,7 +25,7 @@
     <p>A few key points from the 47-minute discussion:</p>
     <h4>
       <ol>
-        <li>Oakland is <a href="http://priceonomics.com/the-most-and-least-diverse-cities-in-america/">by many measures the most diverse city in the United States</a>, but Open Oakland found that they did not meet that standard for representation. Open Oakland tried to build an organization that could change the city and build trust.</li>
+        <li>Oakland is <a href="http://priceonomics.com/the-most-and-least-diverse-cities-in-america/">by many measures the most diverse city in the United States</a>, but Open Oakland found that they did not represent that standard. Open Oakland tried to build an organization that could build trust and change the city.</li>
         <li>Open Oakland's leadership picked 17 people in total to discuss the future of the organization. Their recommended process for a leadership change:
           <ol>
             <li>Curate a few folks who are regularly attending and see whether they are interested in being a leader or helping to craft a leadership structure.</li>
@@ -35,12 +35,14 @@
           </ol>
         </li>
         <li>Open Oakland developed a "SWAT" team to identify roles and people to fill them that would represent Oakland's community. They first identified functional areas, then added roles to cover that work, like partnerships, outreach, development. <a href="http://openoakland.org/jobs/">Visit Open Oakland's website</a> for position descriptions.</li>
-        <li>Open Oakland engaged some seed funding from the Kapor Foundation to get <a href="https://twitter.com/nmsanchez">Nicole Sanchez</a> from Vaya Consulting to help them develop a process to recruit people.</li>
-        <li>They did a lot of broad outreach to find new voices. The relationships among existing Open Oakland volunteers were already close, and they did not want the process to only engage their close associations as this would not help them reach more of Oakland. They used anonymous applications as much as possible, and couldn’t see each other’s hand-written notes or speculations or analysis. Nicole explained this controlled the process, kept our selection based on gut feedback, catalyzed by people’s observations which was a fair assessment. They didn’t know demographic info until they had ranked the roles. The leadership team only analyzed diversity after they had picked the best qualified folks [on paper] for the job. This process resulted in 11 new leaders, a majority of them people of color, several of them queer. However, Open Oakland contends they could have done better with gender diversity.</li>
+        <li>Open Oakland used seed funding from the Kapor Foundation to get <a href="https://twitter.com/nmsanchez">Nicole Sanchez</a> from Vaya Consulting to help them develop a process to recruit leaders.</li>
+        <li>They did a lot of broad outreach to find new voices. The relationships among existing Open Oakland volunteers were already close, and they did not want the process to only engage their close associations as this would not help them reach more of Oakland. They used anonymous applications as much as possible, and couldn’t see each other’s hand-written notes or speculations or analysis. Nicole explained this controlled the process, kept our selection based on gut feedback, catalyzed by people’s observations which was a fair assessment. They didn’t know demographic info until they had ranked the roles. The leadership team only analyzed diversity after they had picked the best qualified folks [on paper] for the job. This process resulted in 11 new leaders, a majority of them people of color, several of them LGBTQ. However, Open Oakland contends they could have done better with gender diversity.</li>
         <li>The big takeaway:
-          <li>ELLIE: We were really thoughtful about it and learned a lot.</li>
-          <li>EDDIE: It takes time, and you have to want to put the effort into doing this kind of work to reach out and bring in different perspectives. It’s much easier to select people who are just like you.</li>
-          <li>SPIKE: Diversity isn’t an idea, it’s an action. It was necessary to do a lot of things to work on this. It was a hard thing to do, but it’s important for us to build a strong base as we get more serious about Open Oakland.</li>
+          <ol>
+           <li>ELLIE: We were really thoughtful about it and learned a lot.</li>
+           <li>EDDIE: It takes time, and you have to want to put the effort into doing this kind of work to reach out and bring in different perspectives. It’s much easier to select people who are just like you.</li>
+           <li>SPIKE: Diversity isn’t an idea, it’s an action. It was necessary to do a lot of things to work on this. It was a hard thing to do, but it’s important for us to build a strong base as we get more serious about Open Oakland.</li>
+           </ol>
         </li>
       </ol>
     </h4>
@@ -55,12 +57,13 @@
 
        <h2>More resources</h2>
        <p>Here are some resources that Open Oakland shared with us.</p>
+        <h4>
       <ol>
         <li><a href="https://docs.google.com/document/d/1mXJAno1oIF_chfr9I0C8wyNSF5_A8naQgBiS_gWr49g/edit?usp=sharing">Email for public recruitment</a></li>
         <li><a href="https://docs.google.com/document/d/1HXZhLbOk6peqyMFAufI0q0YX2F9JDFTCKq7u54pSGdM/edit?usp=sharing">Job descriptions</li>
         <li><a href="https://docs.google.com/document/d/1cQpZb7VBwHE7MlxGtIugjOoDm5m5Gs3wNNt3sph0eu8/edit?usp=sharing">Welcome email to the new team</a></li>
       </ol>
-    </h2>
+        </h4>
 
     </div>
 
